9334cdb55ab0fe4903092f46b30fda214bc657bd
Lesya Ilina <92212122+KerianLk@users.noreply.github.com>
2025-03-28 17:06:04 +0300

    111

--- users/admin.py
+++ users/admin.py
@@ -5,10 +5,10 @@ from .models import User
 class CustomUserAdmin(UserAdmin):
     model = User
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': ('role', 'date_of_birth')}),
+        (None, {'fields': ('role', 'date_of_birth','photo')}),
     )
     add_fieldsets = UserAdmin.add_fieldsets + (
-        (None, {'fields': ('role', 'date_of_birth')}),
+        (None, {'fields': ('role', 'date_of_birth','photo')}),
     )
 
 admin.site.register(User, CustomUserAdmin)
--- users/models.py
+++ users/models.py
@@ -7,14 +7,15 @@ class User(AbstractUser):
         ('doctor', 'Врач'),
         ('admin', 'Администратор'),
     )
-    first_name = models.CharField(max_length=150)
-    last_name = models.CharField(max_length=150)
-    middle_name = models.CharField(max_length=150, blank=True, null=True)
-    phone = models.CharField(max_length=11, blank=True, null=True)
-    rassylka = models.BooleanField(default=False)
-    date_of_birth = models.DateField(null=True, blank=True)
-    email = models.EmailField(unique=True)
-    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='patient')
+    first_name = models.CharField(max_length=150, verbose_name="Имя")
+    last_name = models.CharField(max_length=150, verbose_name="Фамилия")
+    middle_name = models.CharField(max_length=150, blank=True, null=True, verbose_name="Отчество")
+    phone = models.CharField(max_length=11, blank=True, null=True, verbose_name="Телефон")
+    rassylka = models.BooleanField(default=False, verbose_name="Разрешаю присылать мне сообщения со спец. предложениями")
+    date_of_birth = models.DateField(null=True, blank=True, verbose_name="Дата рождения")
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='patient', verbose_name="Роль")
+    photo = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name="Аватарка")
 
     def __str__(self):
-        return f"{self.get_full_name()} ({self.role})"
+        return f"{self.get_full_name()}"
